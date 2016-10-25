5ec0aa24a35d5f703ed2097f19d34f4d4a28ef81
Studiesonline <studiesonline56@gmail.com>
2016-10-25 12:32:29 +1100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,8 +10,56 @@ app.get('/', function (req, res) {
 });
 
 
+var articleOne {
+    title = 'Article One';
+    date = 'Oct 25, 2016';
+    content = `<p>
+                    This is article one. Today is a beautiful sunny day :). Lots of smiles and laughter 
+                </p>
+                <p>
+                    Studying online is cool. Brain getting refreshed at the comfort of your home and a hot cuppa of tea!!
+                </p>`
+}
+
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var content = data.content;
+
+var htmlTemplate =
+     `
+      <html>
+      <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content = "width=device=width , initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class = "container">
+        <div>
+            <a href='/'>Home</a>
+            <hr/>
+            <h3>
+                Article One
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+            </div>
+        </div>
+        </div>
+    </body>
+</html>`
+ 
+return htmlTemplate;  
+} 
+  
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
