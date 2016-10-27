08803b081c68bcd626008153f6de88841f1a7596
Studiesonline <studiesonline56@gmail.com>
2016-10-27 13:22:28 +1100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 //change the main content in loading page 
 var element = document.getElementById('mainText');
 element.innerHTML = 'Hi Welcome to the website' ;
+
+//move the dragon
+
+var img = document.getElementById('dragie');
+img.onclick = function () {
+    img.style.marginleft ='100px';
+    };
