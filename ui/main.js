9ddbc20d1ff3e159af8c97ae20ec450470968536
Studiesonline <studiesonline56@gmail.com>
2016-11-02 11:13:17 +1100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
 
 //change the main content in loading page 
-var element = document.getElementById('mainText');
-element.innerHTML = 'Hi Welcome to the website' ;
+//var element = document.getElementById('mainText');
+//element.innerHTML = 'Hi Welcome to the website' ;
 
 //move the dragon
 
