6f404f85dab529f72484a4f17ab2a8e3eaa3976b
Studiesonline <studiesonline56@gmail.com>
2016-11-02 11:27:35 +1100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+
 
 
 var articles = {
@@ -85,6 +83,17 @@ var htmlTemplate =
  
 return htmlTemplate;  
 } 
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var counter =0;
+app.get('/counter', function(req,res) {
+    
+    counter= counter +1;
+    res.send(counter.toString());
+});
   
 app.get('/:articleName',function(req,res) {
     var articleName = req.params.articleName;
